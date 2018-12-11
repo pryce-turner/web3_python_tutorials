38eb177321334efc38b75c83121e396543d72799
Pryce <pryce.turner@gmail.com>
2018-12-11 17:11:15 +0000

    Added provisioning script and walkthrough file

---
+++ provision_vm.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt-get install -y libssl-dev build-essential automake pkg-config libtool libffi-dev libgmp-dev libyaml-cpp-dev python python3-pip solc
+
+# Install web3
+pip3 install web3 py-solc
+
+# Install node
+echo "Run the following in new terminals to finish provisioning"
+echo "---------------------------------------------------------"
+echo "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash"
+echo "nvm install node"
+echo "npm install -g node-gyp truffle ganache-cli"
+echo "---------------------------------------------------------"
---
+++ walkthrough.py
@@ -0,0 +1,7 @@
+# Put your imports here
+
+# Initialize your web3 object
+
+# Create a path object to your Solidity source files
+
+# Initialize your interface
